2a1dcef0c69e48ef4cc8d3ddce88369ae07cda35
jackloague1 <55412612+jackloague1@users.noreply.github.com>
2022-04-22 23:38:43 -0700

    Add files via upload

--- Algorithm_1.py
+++ Algorithm_1.py
@@ -1,5 +1,7 @@
 def targetWords(Array_A, Array_B):
+    # Displays the starting indices of searched words in Array_A
     Output_order = []
+    # Displays the words found in Array_A
     Output_array = []
     j = 0
 
@@ -8,6 +10,7 @@ def targetWords(Array_A, Array_B):
         j = 0
         # Iterate through each character in Array_A
         for i in range(0, len(Array_A[0])):
+            print(i)
             # If character of Array_A ([0][i]) is equal to character ([j]) of word ([k]) in Array_B
             if Array_A[0][i] == Array_B[k][j]:
                 # Move to next character of word in Array_B to check if it is also found in Array_A
@@ -22,9 +25,16 @@ def targetWords(Array_A, Array_B):
                 Output_array.append(Array_A[0][i-j:i+1])
                 break
     
-    print(Output_order)
-    print(Output_array)
+    print("")
+    print("Starting indices of target words: ", Output_order)
+    print("Target words found: ", Output_array)
 
 Array_1A = ["sanoaklandrialtofullertonmarcolongchinocoronamodestoclovissimithousand"]
 Array_1B = ["oakland", "modesto", "clovis", "corona"]
+Array_2A = ["polmonafremontrialtofullertonmarcolongfresnochinoclovissimisalinas"]
+Array_2B = ["fullerton", "chino", "fremont", "fresno"]
+Array_3A = ["torranceoaklandrialtomarcooxnardchinofresnoirvineclovissimiorange"]
+Array_3B = ["oxnard", "irvine", "orange", "marco"]
 targetWords(Array_1A, Array_1B)
+targetWords(Array_2A, Array_2B)
+targetWords(Array_3A, Array_3B)
